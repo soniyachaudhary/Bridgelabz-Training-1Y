581c64bb8bd9dd6f684906ae931af9eb47e6b77f
Soni Chaudhary <soniichaudhary074@gmail.com>
2026-04-27 17:30:16 +0530

    [soniya] added task

---
+++ Regex/Regex/src/Regex/Question1.java
@@ -0,0 +1,17 @@
+package Regex;
+    import java.util.*;
+    class Question1 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter username: ");
+            String username = sc.nextLine();
+
+            String regex = "^[A-Za-z][A-Za-z0-9_]{4,14}";
+            if (username.matches(regex)) {
+                System.out.println("Valid Username");
+            } else {
+                System.out.println("Invalid Username");
+            }
+        }
+    }
+
